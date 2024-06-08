110ad02d8fc6c91407d3f2bd6303d7a3dd42bd0f
StrangerEE <2277241439@qq.com>
2024-06-08 12:12:34 +0800

    data文件

--- function.py
+++ function.py
@@ -1,35 +1,18 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from pandas import Series
-import statsmodels.api as sm
-from statsmodels.graphics.tsaplots import acf,pacf,plot_acf,plot_pacf
-from statsmodels.tsa.arima_model import ARMA, ARIMA
-from statsmodels.tsa.stattools import adfuller
-from pylab import mpl
-mpl.rcParams['font.sans-serif']=['SimHei']
-mpl.rcParams['axes.unicode_minus']=False
+import numpy as np
 
+def Remove_outliers(data):
+    mean = data['性能'].mean()
+    std_dev = data['性能'].std()
 
-# 绘制移动平均图
-def draw_trend(timeseries, size):
-    f = plt.figure(facecolor='white')
-    # 对size个数据进行移动平均
-    rol_mean = timeseries.rolling(window=size).mean()
-    # 对size个数据移动平均的方差
-    rol_std = timeseries.rolling(window=size).std()
-    timeseries.plot(color='blue', label=u'原始数据')
-    rol_mean.plot(color='red', label=u'移动平均')
-    rol_std.plot(color='black', label=u'移动平均标准差')
-    plt.legend(loc='best')
-    plt.title('移动平均值和标准差')
-    plt.show()  # 移动这行代码到这里
-    return f
+    # 定义阈值，例如，超过均值加减2倍标准差的点视为离群点
+    threshold = 2 * std_dev
 
-#Dickey-Fuller 检验:
-def stationarity(ts):
-    dftest = adfuller(ts)
-    # 对上述函数求得的值进行语义描述
-    dfoutput = pd.Series(dftest[0:4], index=['t统计值','p值','#滞后项','观测点'])
-    for key,value in dftest[4].items():
-        dfoutput['关键值 (%s)'%key] = value
-    return dfoutput
+    # 根据阈值删除离群点
+    cleaned_data = data[(data['性能'] >= mean - threshold) & (data['性能'] <= mean + threshold)]
+
+    return cleaned_data
+
+# Define the model function, for example, a quadratic function
+def model_func(x, a, b, c):
+    return a * x**2 + b * x + c
